b8cf8499aa036cec2566697557eee92fc0f4ba73
Mehul Mohan <mehulmpt@gmail.com>
2020-12-10 01:30:23 +0530

    lab added

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-function whatIsInAName(collection, source) {
-  var arr = [];
-  // Only change code below this line
-
-
-  // Only change code above this line
-  return arr;
+function spinalCase(str) {
+  return str;
 }
 
-whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+spinalCase('This Is Spinal Tap');
