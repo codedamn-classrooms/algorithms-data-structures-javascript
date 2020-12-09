54b4114f6e3855503611819da68523a5efdde067
Mehul Mohan <mehulmpt@gmail.com>
2020-12-10 01:30:24 +0530

    lab added

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-var Person = function(firstAndLast) {
-  // Only change code below this line
-  // Complete the method below and implement the others similarly
-  this.getFullName = function() {
-    return "";
-  };
-  return firstAndLast;
-};
+function orbitalPeriod(arr) {
+  var GM = 398600.4418;
+  var earthRadius = 6367.4447;
+  return arr;
+}
 
-var bob = new Person('Bob Ross');
-bob.getFullName();
+orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
