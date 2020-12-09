8be8f31c803f141d97b986e554dcabc5c36e58b7
Mehul Mohan <mehulmpt@gmail.com>
2020-12-10 01:26:12 +0530

    lab added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function largestOfFour(arr) {
-  return arr;
+function confirmEnding(str, target) {
+  return str;
 }
 
-largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+confirmEnding("Bastian", "n");
