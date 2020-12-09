040a4061bb3c8b1f7eb658c5a13bb043996ef838
Mehul Mohan <mehulmpt@gmail.com>
2020-12-10 01:17:53 +0530

    lab added

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-let myArray = ["a", "b", "c", "d"];
-// Only change code below this line
+function mixedNumbers(arr) {
+  // Only change code below this line
 
-// Only change code above this line
-console.log(myArray);
+  // Only change code above this line
+  return arr;
+}
+
+console.log(mixedNumbers(['IV', 5, 'six']));
