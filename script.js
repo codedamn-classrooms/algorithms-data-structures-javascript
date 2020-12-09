df9bb5a03396060904c5618ff4524926ed737702
Mehul Mohan <mehulmpt@gmail.com>
2020-12-10 01:17:53 +0530

    lab added

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-function forecast(arr) {
-  // Only change code below this line
+function copyMachine(arr, num) {
+  let newArr = [];
+  while (num >= 1) {
+    // Only change code below this line
 
-  return arr;
+    // Only change code above this line
+    num--;
+  }
+  return newArr;
 }
 
-// Only change code above this line
-console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
+console.log(copyMachine([true, false, true], 2));
