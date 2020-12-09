5cce91057f7c02ae3ee2778130d4562c798e2512
Mehul Mohan <mehulmpt@gmail.com>
2020-12-10 01:17:53 +0530

    lab added

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-function copyMachine(arr, num) {
-  let newArr = [];
-  while (num >= 1) {
-    // Only change code below this line
-
-    // Only change code above this line
-    num--;
-  }
-  return newArr;
+function spreadOut() {
+  let fragment = ['to', 'code'];
+  let sentence; // Change this line
+  return sentence;
 }
 
-console.log(copyMachine([true, false, true], 2));
+console.log(spreadOut());
