bec1d2ac9c00d1ed891e61842f11ea3274ea728b
Mehul Mohan <mehulmpt@gmail.com>
2020-12-10 01:17:53 +0530

    lab added

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function filteredArray(arr, elem) {
-  let newArr = [];
+let myNestedArray = [
   // Only change code below this line
-
+  ['unshift', false, 1, 2, 3, 'complex', 'nested'],
+  ['loop', 'shift', 6, 7, 1000, 'method'],
+  ['concat', false, true, 'spread', 'array'],
+  ['mutate', 1327.98, 'splice', 'slice', 'push'],
+  ['iterate', 1.3849, 7, '8.4876', 'arbitrary', 'depth']
   // Only change code above this line
-  return newArr;
-}
-
-console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
+];
