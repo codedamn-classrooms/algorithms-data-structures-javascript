a7cd161c315a86718ffadea54c0e6407b6525856
Mehul Mohan <mehulmpt@gmail.com>
2020-12-10 01:26:12 +0530

    lab added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function findLongestWordLength(str) {
-  return str.length;
+function largestOfFour(arr) {
+  return arr;
 }
 
-findLongestWordLength("The quick brown fox jumped over the lazy dog");
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
