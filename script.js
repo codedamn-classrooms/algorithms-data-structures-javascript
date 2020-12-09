3f10f57a8fa17c10b008f00624436a070fc9140a
Mehul Mohan <mehulmpt@gmail.com>
2020-12-10 01:30:23 +0530

    lab added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function myReplace(str, before, after) {
+function pairElement(str) {
   return str;
 }
 
-myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
+pairElement("GCG");
