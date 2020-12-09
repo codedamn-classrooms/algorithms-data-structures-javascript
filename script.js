6ec10b7274a97afc374956ac6eaf53edcb9ba816
Mehul Mohan <mehulmpt@gmail.com>
2020-12-10 01:30:24 +0530

    lab added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function truthCheck(collection, pre) {
-  return pre;
+function addTogether() {
+  return false;
 }
 
-truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
+addTogether(2,3);
