5abeb1d1c22e198061f41a6964c74b29ae6ff9c4
Mehul Mohan <mehulmpt@gmail.com>
2020-12-10 01:17:54 +0530

    lab added

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-function countOnline(usersObj) {
+let users = {
+  Alan: {
+    age: 27,
+    online: false
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: false
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+function getArrayOfUsers(obj) {
   // Only change code below this line
 
   // Only change code above this line
 }
+
+console.log(getArrayOfUsers(users));
